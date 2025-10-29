5a8cc76a1c83a2154d040cdaf130775132e29d7b
Faridyrg16 <faridyrg16@gmail.com>
2025-10-28 20:55:22 -0500

    Update ejercios pthon.py

--- Desarrolo de Backend 5F/ejercios pthon.py
+++ Desarrolo de Backend 5F/ejercios pthon.py
@@ -5,10 +5,12 @@
 4- crea una funcion que devuelva true si el numero es par y false si es impar
 5- escribir una funcion  que determine si una palabra es palindroma o sea si se lee igual de izquierda a derecha y de derecha a izquierda debe validar que no haya espacios en blanco y no distinga entre mayusculas y minusculas
 '''
+# librerias
 import random
 # se declara variables
 numero1 = int(input("Introduce el primer número: "))
 numero2 = int(input("Introduce el segundo número: "))
+numero3 = int(input("Introduce el tercer número: "))
 # Creamos función para encontrar el mayor
 def encontrar_mayor(num1, num2):
   if num1 > num2:
@@ -18,8 +20,32 @@ def encontrar_mayor(num1, num2):
 mayor = encontrar_mayor(numero1, numero2)
 print(f"El número mayor es: {mayor}")
 
+def suma_de_tres(a, b, c):
+    return numero1 + numero2 + numero3
+
+print(suma_de_tres(numero1, numero2, numero3))
+
 def mensaje_aleatorio():
     mensajes = ["Hola", "Bienvenido", "Adiós", "Gracias", "Buen día", "¿Cómo estás?", "Éxitos", "Felicidades", "Nos vemos", "Hasta pronto"]
     return random.choice(mensajes)
 
 print(mensaje_aleatorio())
+
+def es_par(numero):
+    if numero % 2 == 0:
+        return f"El número {numero} es par"
+    else:
+        return f"El número {numero} es impar"
+
+print(es_par(numero1))
+print(es_par(numero2))
+print(es_par(numero3))
+
+def es_palindromo(palabra):
+    if palabra := palabra.replace(" ", "").lower():
+        return palabra == palabra[::-1]
+        f"El número {palabra} es palíndromo"
+    return f"El texto '{palabra}' no es palíndromo."
+palabra = input("Introduce una palabra o frase: ")
+if es_palindromo(palabra):
+    print(f'"{palabra}" es un palíndromo.')

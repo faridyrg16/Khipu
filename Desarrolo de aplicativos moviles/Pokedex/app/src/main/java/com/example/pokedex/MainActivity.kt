2bc4c850a7dc3330134d0f9fe13fc8fd6fef9309
Faridyrg16 <faridyrg16@gmail.com>
2025-12-12 18:24:35 -0500

    funcional

--- Desarrolo de aplicativos moviles/Pokedex/app/src/main/java/com/example/pokedex/MainActivity.kt
+++ Desarrolo de aplicativos moviles/Pokedex/app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -15,12 +15,12 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         val pokemonList = listOf(
-            Pokemon("Caterpie", ""),
-            Pokemon("Larvitar", ""),
-            Pokemon("Nidorino", ""),
-            Pokemon("Quilava", ""),
-            Pokemon("Slugma", ""),
-            Pokemon("Typhlosion", "")
+            Pokemon("Caterpie", "https://images.wikidexcdn.net/mwuploads/wikidex/7/74/latest/20220318232211/EP1191_Caterpie_de_Goh.png"),
+            Pokemon("Larvitar", "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2025/10/how-to-get-evolve-larvitar-in-pokemon-legends-z-a.jpg"),
+            Pokemon("Nidorino", "https://static.wikia.nocookie.net/pokegengame/images/7/7a/Nidorino.png/revision/latest?cb=20140106191027"),
+            Pokemon("Quilava", "https://w1.pngwing.com/pngs/864/693/png-transparent-flame-quilava-cyndaquil-typhlosion-video-games-playing-card-bird-beak.png"),
+            Pokemon("Slugma", "https://kurapixel.com/wp-content/uploads/2024/10/0218Slugma-1.png"),
+            Pokemon("Typhlosion", "https://static.pokemonpets.com/images/monsters-images-800-800/2157-Shiny-Typhlosion.webp")
         )
 
         val adapter = PokemonAdapter(pokemonList)
